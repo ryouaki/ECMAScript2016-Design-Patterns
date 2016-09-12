fa08b11a63c2d8966c03535f4096d3be8ef15c20
YaHuiLiang <yahuiliang@ICs-MacBook-Pro.local>
2016-09-12 11:39:07 +0800

    update abstractfactory

---
+++ AbstractFactory.js
@@ -0,0 +1,94 @@
+'use strict';
+
+class AbstractFactory {
+    constructor() {
+    }
+
+    createProductA (product) {
+    }
+
+    createProductB (product) {
+    }
+}
+
+class ConcreteFactory1 extends AbstractFactory {
+    constructor() {
+        super()
+        console.log("ConcreteFactory1 class created");
+    }
+
+    createProductA (product) {
+        console.log('ConcreteFactory1 createProductA')
+        return new ProductA1()
+    }
+
+    createProductB (product) {
+        console.log('ConcreteFactory1 createProductB')
+        return new ProductB1()
+    }
+}
+
+class ConcreteFactory2 extends AbstractFactory {
+    constructor() {
+        super()
+        console.log("ConcreteFactory2 class created");
+    }
+
+    createProductA (product) {
+        console.log('ConcreteFactory2 createProductA')
+        return new ProductA2()
+    }
+
+    createProductB (product) {
+        console.log('ConcreteFactory2 createProductB')
+        return new ProductB2()
+    }
+}
+
+class AbstractProductA {
+    constructor() {
+    }
+}
+
+class AbstractProductB {
+    constructor() {
+    }
+}
+
+
+class ProductA1 extends AbstractProductA {
+    constructor() {
+        super()
+        console.log('ProductA1 created')
+    }
+}
+
+class ProductA2 extends AbstractProductA {
+    constructor() {
+        super()
+        console.log('ProductA2 created')
+    }
+}
+
+class ProductB1 extends AbstractProductB {
+    constructor() {
+        super()
+        console.log('ProductB1 created')
+    }
+}
+
+class ProductB2 extends AbstractProductB {
+    constructor() {
+        super()
+        console.log('ProductB2 created')
+    }
+}
+
+
+var factory1 = new ConcreteFactory1()
+var productB1 = factory1.createProductB()
+var productA1 = factory1.createProductA()
+
+var factory2 = new ConcreteFactory2()
+var productA2 = factory2.createProductA()
+var productB2 = factory2.createProductB()

91952f888c4c06f8a97d6afc13116b41c3a9bf66
YaHuiLiang <yahuiliang@ICs-MacBook-Pro.local>
2016-09-12 13:07:32 +0800

    Builder

---
+++ Builder.js
@@ -0,0 +1,54 @@
+'use strict';
+
+class Builder {
+    constructor() {
+        console.log('Builder Class created!');
+    }
+
+    buildPart(partName) {
+        console.log('Builder.buildPart invoked!');
+    }
+}
+
+class ConcreteBuilder extends Builder {
+    constructor() {
+        super();
+        console.log('ConcreteBuilder Class created!');
+    }
+
+    buildPart(partName) {
+        super.buildPart(partName);
+        console.log('ConcreteBuilder.buildPart invoked!');
+        this.product = new Product(partName);
+    }
+    getResult() {
+        console.log('ConcreteBuilder.getResult invoked!');
+        return this.product;
+    }
+}
+
+class Product {
+    constructor(material) {
+        console.log("Product class created");
+        this.data = material
+    }
+}
+
+class Director {
+    constructor() {
+        this.structure = ['Prod1', 'Prod2', 'Prod3'];
+        console.log("Director class created");
+    }
+
+    Construct() {
+        console.log("Director.Construct created");
+        for (var prodName in this.structure) {
+            let builder = new ConcreteBuilder();
+            builder.buildPart(this.structure[prodName]);
+            builder.getResult();
+        }
+    }
+}
+
+let director = new Director();
+director.Construct();

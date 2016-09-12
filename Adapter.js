6372110d2b0f9e3c78a596c441f297f421be34be
YaHuiLiang <yahuiliang@ICs-MacBook-Pro.local>
2016-09-12 15:50:11 +0800

    add adapter

---
+++ Adapter.js
@@ -0,0 +1,47 @@
+'use strict';
+
+class Target {
+    constructor(type) {
+        console.log('Target Class created!');
+        let result = undefined;
+
+        switch (type) {
+            case 'adapter':
+                result = new AdapterImpl();
+                break
+            default:
+                result = undefined;
+        }
+        return result;
+    }
+
+    request() {
+        console.log('Target.request created');
+    }
+}
+
+class Adapter {
+    constructor() {
+        console.log('Adapter Class created');
+    }
+
+    specificRequest() {
+        console.log('Adapter.specificRequest invoked');
+    }
+}
+
+class AdapterImpl extends Adapter {
+
+    constructor() {
+        super()
+        console.log('AdapterImpl Class created');
+    }
+
+    request() {
+        console.log('AdapterImpl.request created');
+        return this.specificRequest();
+    }
+}
+
+var f = new Target("adapter");
+f.request();
